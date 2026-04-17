160834022a30df81b44988573c5884f477fbce60
cbadenes <cbadenes@gmail.com>
2026-04-17 07:19:58 +0200

    updated to latest version of numpy

--- scripts/utils.py
+++ scripts/utils.py
@@ -252,7 +252,7 @@ def convert_to_serializable(obj):
         return bool(obj)
     elif isinstance(obj, (np.int_, np.intc, np.intp, np.int8, np.int16, np.int32, np.int64)):
         return int(obj)
-    elif isinstance(obj, (np.float_, np.float16, np.float32, np.float64)):
+    elif isinstance(obj, (np.floating,)):
         return float(obj)
     elif isinstance(obj, (np.ndarray,)):
         return obj.tolist()
